93231202c36c170412b8bf0898f11681159c8707
Attila135 <magyarosia.25ad@acsjszki.hu>
2026-03-25 17:24:57 +0100

    1

---
+++ java.js
@@ -0,0 +1,14 @@
+function saa() {
+    let x = document.getElementById("x");
+    //let x = 894651684
+    while (x != 1 & x != 0) {
+        if (x % 2 == 1) {
+            x = 3 * x + 1;
+        } else {
+            x = x / 2;
+        }
+        console.log(x);
+        document.getElementById("y").innerHTML = x.innerHTML;
+    }
+
+}
